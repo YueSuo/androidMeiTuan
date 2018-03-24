9b1914ec750f9b65fa08854db0f036742451963a
YueSuo <923821545@qq.com>
2018-03-24 22:42:59 +0800

    美团app页面

---
+++ MeiTuan/app/src/main/java/com/example/sy/meituan/meiTuan.java
@@ -0,0 +1,17 @@
+package com.example.sy.meituan;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by suoyue on 2018/3/24.
+ */
+
+public class meiTuan extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.meituan);
+    }
+}
